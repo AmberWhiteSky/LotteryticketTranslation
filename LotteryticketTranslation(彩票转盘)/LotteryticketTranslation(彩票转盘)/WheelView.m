da5f66aed996ce0cb14cec98ef81b06fde1692dd
AmberWhiteSky <amberwhitesky@163.com>
2015-10-16 23:40:40 +0800

    添加了12个按钮 并设置点击事件

--- LotteryticketTranslation(彩票转盘)/LotteryticketTranslation(彩票转盘)/WheelView.m
+++ LotteryticketTranslation(彩票转盘)/LotteryticketTranslation(彩票转盘)/WheelView.m
@@ -33,22 +33,51 @@
 }
 
 
-- (instancetype)initWithCoder:(NSCoder *)coder
-{
-    self = [super initWithCoder:coder];
-    if (self) {
-        //添加十二个按钮
-        for(int  index =0;index<12;index++){
-            UIButton  *btn =[UIButton  buttonWithType:UIButtonTypeCustom];
-            btn.backgroundColor =[UIColor  redColor];
-            [btn setBackgroundImage: [UIImage  imageNamed:@"LuckyRototeSelected"] forState:UIControlStateSelected];
-//            btn.frame =
-            [self.centerView   addSubview:btn];
-        }
+
+- (void)awakeFromNib {
+    
+    
+    self.centerView.userInteractionEnabled =YES;
+    //添加十二个按钮
+    for(int  index =0;index<12;index++){
+        UIButton  *btn =[UIButton  buttonWithType:UIButtonTypeCustom];
+        btn.backgroundColor =[UIColor  colorWithRed:arc4random_uniform(255)/255.0 green:arc4random_uniform(255)/255.0  blue:arc4random_uniform(255)/255.0  alpha:1.0];
         
+        [btn setBackgroundImage: [UIImage  imageNamed:@"LuckyRototeSelected"] forState:UIControlStateSelected];
+        btn.bounds =CGRectMake(0, 0, 68, 143);
+        //设置锚点和位置
+        btn.layer.anchorPoint = CGPointMake(0.5, 1);
+        btn.layer.position =CGPointMake(self.centerView.frame.size.width*0.5, self.centerView.frame.size.height*0.5);
+        CGFloat angle =(30*index)/180.0*M_PI;
+        btn.transform =CGAffineTransformMakeRotation(angle);
+        
+        //监听按钮点击
+        [btn addTarget:self action:@selector(bthClick:) forControlEvents:UIControlEventTouchUpInside];
+        
+        [self.centerView   addSubview:btn];
+
+
     }
-    return self;
 }
+
+-(void)bthClick :(UIButton *)  btn {
+    btn.selected=YES;
+}
+//- (instancetype)initWithCoder:(NSCoder *)coder{
+//    self = [super initWithCoder:coder];
+//    if (self) {
+//        //添加十二个按钮
+//        for(int  index =0;index<12;index++){
+//            UIButton  *btn =[UIButton  buttonWithType:UIButtonTypeCustom];
+//            btn.backgroundColor =[UIColor  redColor];
+//            [btn setBackgroundImage: [UIImage  imageNamed:@"LuckyRototeSelected"] forState:UIControlStateSelected];
+////            btn.frame =
+//            [self.centerView   addSubview:btn];
+//        }
+//        
+//    }
+//    return self;
+//}
 
 
 
@@ -69,5 +98,6 @@
     [self.centerView.layer  addAnimation:anim forKey:nil];
 
 }
+    
 
 @end
